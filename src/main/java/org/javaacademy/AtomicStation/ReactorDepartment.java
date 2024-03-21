16680d2e23777de1c143ef6fe23bfe22cdcf6601
dash01 <damir@shakirov.tv>
2024-03-21 23:15:27 +0300

    Исправлены ошибки по чекстайлу

--- src/main/java/org/javaacademy/AtomicStation/ReactorDepartment.java
+++ src/main/java/org/javaacademy/AtomicStation/ReactorDepartment.java
@@ -1,6 +1,5 @@
 package org.javaacademy.AtomicStation;
 
-import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
--- src/test/java/org/javaacademy/AtomicStation/AtomicStationApplicationTests.java
+++ src/test/java/org/javaacademy/AtomicStation/AtomicStationApplicationTests.java
@@ -1,6 +1,5 @@
 package org.javaacademy.AtomicStation;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
