07db4a168797f3b96615ab04615d5bdb2b8301f1
dash01 <damir@shakirov.tv>
2024-03-21 15:36:16 +0300

    start project

---
+++ src/main/java/org/javaacademy/AtomicStation/AtomicStationApplication.java
@@ -0,0 +1,12 @@
+package org.javaacademy.AtomicStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicStationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicStationApplication.class, args);
+	}
+
+}
